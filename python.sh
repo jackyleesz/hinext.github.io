825d8af668dc43d8d8928bfbd89ac4366fc2fa72
ovocat <30361541+ovocat@users.noreply.github.com>
2017-08-04 18:19:23 +0800

    v1.2

--- python.sh
+++ python.sh
@@ -1,65 +1,21 @@
-#!/bin/bash
-# a script to install python 2.7 on CentOS 6.x system.
-# CentOS 6.x has python 2.6 by default, while some software (e.g. django1.7)
-# need python 2.7.
- 
-# install some necessary tools & libs
-echo "install some necessary tools & libs"
-yum groupinstall "Development tools" -y
-yum install openssl-devel zlib-devel ncurses-devel bzip2-devel readline-devel -y
-yum install libtool-ltdl-devel sqlite-devel tk-devel tcl-devel -y
-sleep 5
- 
-# download and install python
-version='2.7.8'
-python_url="https://www.python.org/ftp/python/$version/Python-${version}.tgz"
- 
-# check current python version
-echo "before installation, your python version is: $(python -V &2>1)"
-python -V 2>&1 | grep "$version"
-if [ $? -eq 0 ]; then
-  echo "current version is the same as this installation."
-  echo "Quit as no need to install."
-  exit 0
-fi
- 
-echo "download/build/install your python"
-cd /tmp
-wget $python_url
-tar -zxf Python-${version}.tgz
-cd Python-${version}
-./configure
-make -j 4
-make install
-sleep 5
- 
-echo "check your installed python"
-python -V 2>&1 | grep "$version"
-if [ $? -ne 0 ]; then
-  echo "python -V is not your installed version"
-  /usr/local/bin/python -V 2>&1 | grep "$version"
-  if [ $? -ne 0 ]; then
-    echo "installation failed. use '/usr/local/bin/python -V' to have a check"
-  fi
-  exit 1
-fi
-sleep 5
- 
-# install setuptools
-echo "install setuptools"
-wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
-python ez_setup.py
-# check easy_install version
-easy_install --version
-sleep 5
- 
-# install pip for the new python
-echo "install pip for the new python"
-easy_install pip
-# check pip version
-pip -V
- 
-echo "Finished. Well done!"
-echo "If 'python -V' still shows the old version, you may need to re-login."
-echo "And/or set /usr/local/bin in the front of your PATH environment variable."
-echo "-------------------------"
+#!/bin/bash  
+#  
+#  Author: hyxc  
+#  
+##########################安装依赖包#  
+yum -y install python-devel openssl-devel gcc gcc-c++ sqlite-devel readline-devel ncurses-devel bzip2-devel tk-devel  
+##########################下载python源码包#  
+cd /usr/src/  
+wget https://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz  
+#########################编译安装python#  
+tar zxvf Python-2.7.5.tgz  
+cd Python-2.7.5  
+./configure --prefix=/usr/local/python2.7.5  
+make  
+make install  
+#######################替换之前python#  
+mv /usr/bin/python /usr/bin/python.bak  
+ln -s /usr/local/python2.7.5/bin/python2.7 /usr/bin/python  
+echo 'export PATH=$PATH:/usr/local/python2.7.5/bin/' >> /etc/profile  
+source /etc/profile  
+sed -i 's/#!\/usr\/bin\/python/#!\/usr\/bin\/python2.6/' /usr/bin/yum
